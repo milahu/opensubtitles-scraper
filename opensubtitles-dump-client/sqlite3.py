c09f7a41e6687517fb1f12369ad2a3e3a6576d2a
Milan Hauth <milahu@gmail.com>
2023-03-31 20:57:54 +0200

    mv to opensubtitles-dump-client/sqlite3.ksy

--- opensubtitles-dump-client/sqlite3.py
+++ opensubtitles-dump-client/sqlite3.py
@@ -8,7 +8,7 @@ from enum import Enum
 if getattr(kaitaistruct, 'API_VERSION', (0, 9)) < (0, 9):
     raise Exception("Incompatible Kaitai Struct Python API: 0.9 or later is required, but you have %s" % (kaitaistruct.__version__))
 
-import vlq_base128_be
+from . import vlq_base128_be
 class Sqlite3(KaitaiStruct):
     """SQLite3 is a popular serverless SQL engine, implemented as a library
     to be used within other applications. It keeps its databases as
