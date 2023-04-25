d4092c77682327773afc3ee1a49c9f025577adb3
Milan Hauth <milahu@gmail.com>
2023-04-25 21:05:39 +0200

    up fetch-subs-progress.sh

--- fetch-subs-progress.sh
+++ fetch-subs-progress.sh
@@ -6,4 +6,8 @@ num_total=1000000
 
 progress=$(echo $num_done $num_total | awk '{ print $1/$2*100 }')
 
-echo $(date +"%F %T") $progress%
+size=$(du -sm new-subs 2>/dev/null | cut -d$'\t' -f1)
+
+projected_size=$(echo $size $progress | awk '{ print int($1/($2/100)) }')
+
+echo $(date +"%F %T") $progress% = ${size}MB of ${projected_size}MB
