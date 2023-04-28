9d0c3d3f1c9245cf844740eb8b04033dd0dc216a
Milan Hauth <milahu@gmail.com>
2023-04-28 19:28:46 +0200

    add new-subs-missing-files.py

---
+++ new-subs-missing-files.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+input_dir = "new-subs"
+
+import os
+
+nums = []
+for filename in os.listdir(input_dir):
+    try:
+        num = int(filename.split(".", 1)[0])
+    except ValueError:
+        continue
+    nums.append(num)
+
+nums = sorted(nums)
+
+for idx in range(0, len(nums) - 1):
+    if nums[idx] + 1 == nums[idx + 1]:
+        continue
+    if nums[idx + 1] == 9511453: # outlier
+        break
+    #missing_len = nums[idx + 1] - nums[idx] + 1
+    #print(f"# range from {nums[idx] + 1} to {nums[idx + 1] - 1} = {missing_len} nums")
+    for num in range(nums[idx] + 1, nums[idx + 1]):
+        print(num)
