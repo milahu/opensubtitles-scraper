fc102884135883bfb14feb51736039bee8d17964
Milan Hauth <milahu@gmail.com>
2023-11-20 08:29:12 +0100

    add

---
+++ fetch-subs-chromium.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+exec ./fetch-subs.py --proxy-provider chromium --num-downloads 2 --first-num 9756545 --debug
---
+++ fetch-subs-zenrows.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec ./fetch-subs.py --proxy-provider zenrows.com --num-downloads 2 --first-num 9539186 --debug
