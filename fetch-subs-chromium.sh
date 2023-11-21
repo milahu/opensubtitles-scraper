1bbcefb416e8d7dade021ddb559426ce79c07a00
Milan Hauth <milahu@gmail.com>
2023-11-21 21:44:43 +0100

    fetch and push

--- fetch-subs-chromium.sh
+++ fetch-subs-chromium.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
 
-exec ./fetch-subs.py --proxy-provider chromium --num-downloads 2 --first-num 9756545 --debug --start-vnc-client
+./fetch-subs.py --proxy-provider chromium --num-downloads 10 --first-num 9756545 --debug --start-vnc-client
+
+add_files=./new-subs-repo/add-files.sh
+if [ -x "$add_files" ]; then
+  "$add_files" /run/user/$(id -u)/*-fetch-subs*home/Downloads/*.zip
+fi
