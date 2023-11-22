2ccaed2d8407660251a3f0dddc5f44b54f97d719
Milan Hauth <milahu@gmail.com>
2023-11-22 19:30:53 +0100

    up fetch-subs-chromium.sh

--- fetch-subs-chromium.sh
+++ fetch-subs-chromium.sh
@@ -1,8 +1,16 @@
 #! /usr/bin/env bash
 
-./fetch-subs.py --proxy-provider chromium --num-downloads 10 --first-num 9756545 --debug --start-vnc-client
+num_downloads=10
+num_downloads=2
+
+./fetch-subs.py --proxy-provider chromium --num-downloads $num_downloads --first-num 9756545 --debug --start-vnc-client
 
 add_files=./new-subs-repo/add-files.sh
 if [ -x "$add_files" ]; then
-  "$add_files" /run/user/$(id -u)/*-fetch-subs*home/Downloads/*.zip
+  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/Downloads/*.zip
+  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/Downloads/*.not-found
+  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/done_downloads/req*/*.zip
+  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/done_downloads/req*/*.not-found
+  "$add_files" new-subs/*.zip
+  "$add_files" new-subs/*.not-found
 fi
