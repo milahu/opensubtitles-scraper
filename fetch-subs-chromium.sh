38e26cce0668ec019dce5a0f7af2378aa2eb9b29
Milan Hauth <milahu@gmail.com>
2023-12-22 15:14:03 +0100

    add fetch-subs-chromium.sh

--- fetch-subs-chromium.sh
+++ fetch-subs-chromium.sh
@@ -1,16 +1,9 @@
 #! /usr/bin/env bash
 
-num_downloads=10
-num_downloads=2
+# daily quota for non-vip accounts
+# can be bypassed by changing the IP address
+num_downloads=200
 
 ./fetch-subs.py --proxy-provider chromium --num-downloads $num_downloads --first-num 9756545 --debug --start-vnc-client
 
-add_files=./new-subs-repo/add-files.sh
-if [ -x "$add_files" ]; then
-  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/Downloads/*.zip
-  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/Downloads/*.not-found
-  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/done_downloads/req*/*.zip
-  "$add_files" /run/user/$(id -u)/fetch-subs-*/home/done_downloads/req*/*.not-found
-  "$add_files" new-subs/*.zip
-  "$add_files" new-subs/*.not-found
-fi
+./fetch-subs-add-zipfiles.sh
