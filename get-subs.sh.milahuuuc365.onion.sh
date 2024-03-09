f370c3c8b139d60f79bf9b355cda862837f6f938
Milan Hauth <milahu@gmail.com>
2024-03-09 20:26:57 +0100

    up get-subs.sh.milahuuuc365.onion.sh

--- get-subs.sh.milahuuuc365.onion.sh
+++ get-subs.sh.milahuuuc365.onion.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # get-subs.sh - get subtitles from subtitles server
 server_url="http://milahuuuc3656fettsi3jjepqhhvnuml5hug3k7djtzlfe4dw6trivqd.onion/bin/get-subtitles"
+# note: this requires a running tor proxy on 127.0.0.1:9050 - hint: sudo systemctl start tor
 curl=(curl --proxy socks5h://127.0.0.1:9050)
 command -v curl >/dev/null || { echo "error: curl was not found"; exit 1; }
 command -v unzip >/dev/null || { echo "error: unzip was not found"; exit 1; }
@@ -10,9 +11,17 @@ dir="$(dirname "$1")"
 cd "$dir"
 movie="$(basename "$1")"
 if command -v bsdtar >/dev/null; then
-  "${curl[@]}" -G -o - -d "movie=$movie" "$server_url" | bsdtar -xvf -
+  # https://superuser.com/a/1834410/951886 # write error body to stderr
+  "${curl[@]}" -G --fail-with-body -D - -o - --data-urlencode "movie=$movie" "$server_url" | {
+    s=; while read -r h; do h="${h:0: -1}"; if [ -z "$s" ]; then s=${h#* }; s=${s%% *}; fi; [ -z "$h" ] && break; done
+    if [ "${s:0:1}" = 2 ]; then cat; else cat >&2; fi
+  } | bsdtar -xvf -
 else
   zip="${movie%.*}.subs.zip"
   ! [ -e "$zip" ] || { echo "error: tempfile exists: ${zip@Q}"; exit 1; }
-  "${curl[@]}" -G -o "$zip" -d "movie=$movie" "$server_url" && unzip "$zip" && rm "$zip"
+  if ! "${curl[@]}" -G --fail-with-body -o "$zip" --data-urlencode "movie=$movie" "$server_url"; then
+    cat "$zip" && rm "$zip" # zip contains the error message
+  else
+    unzip "$zip" && rm "$zip"
+  fi
 fi
