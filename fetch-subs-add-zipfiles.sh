21912efbeb668641723462a433bd6675cf3b12d6
Milan Hauth <milahu@gmail.com>
2025-08-05 20:44:15 +0200

    fix new_subs_dir

--- fetch-subs-add-zipfiles.sh
+++ fetch-subs-add-zipfiles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 new_subs_repo_shards_dir="opensubtitles-scraper-new-subs"
+new_subs_dir="new-subs"
 
 add_files="./$new_subs_repo_shards_dir/add-files.sh"
 if [ -x "$add_files" ]; then
@@ -8,6 +9,6 @@ if [ -x "$add_files" ]; then
   "$add_files" /run/user/$(id -u)/fetch-subs-*/home/Downloads/*.not-found
   "$add_files" /run/user/$(id -u)/fetch-subs-*/home/done_downloads/req*/*.zip
   "$add_files" /run/user/$(id -u)/fetch-subs-*/home/done_downloads/req*/*.not-found
-  "$add_files" new-subs/*.zip
-  "$add_files" new-subs/*.not-found
+  "$add_files" "$new_subs_dir"/*.zip
+  "$add_files" "$new_subs_dir"/*.not-found
 fi
