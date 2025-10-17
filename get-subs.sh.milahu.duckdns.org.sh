cf9a4a3f3c7adbc163682948f86716a0391f1d74
Milan Hauth <milahu@gmail.com>
2025-10-17 09:47:33 +0200

    add get-subs.sh.milahu.duckdns.org.sh

---
+++ get-subs.sh.milahu.duckdns.org.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# get-subs.sh - get subtitles from subtitles server
+#set -x # xtrace
+server_url="http://milahu.duckdns.org/bin/get-subtitles"
+curl=(curl)
+command -v curl >/dev/null || { echo "error: curl was not found"; exit 1; }
+command -v unzip >/dev/null || { echo "error: unzip was not found"; exit 1; }
+[ -n "$1" ] || { echo "usage: $0 [--lang en,es,de,ru,cn] path/to/Scary.Movie.2000.720p.mp4"; exit 1; }
+lang=
+while (( $# > 0 )); do
+case "$1" in
+  --lang|-l) lang="$2"; shift 2; continue;;
+  *) :;;
+esac
+dir="$(dirname "$1")"
+[ -e "$dir" ] || { echo "error: no such directory: ${dir@Q}"; exit 1; }
+pushd "$dir" >/dev/null
+movie="$(basename "$1")"
+curl_data=(
+  --data-urlencode "movie=$movie"
+  --data-urlencode "lang=$lang"
+)
+if command -v bsdtar >/dev/null; then
+  # https://superuser.com/a/1834410/951886 # write error body to stderr
+  "${curl[@]}" -G --fail-with-body -D - -o - "${curl_data[@]}" "$server_url" | {
+    s=; while read -r h; do h="${h:0: -1}"; if [ -z "$s" ]; then s=${h#* }; s=${s%% *}; fi; [ -z "$h" ] && break; done
+    if [ "${s:0:1}" = 2 ]; then cat; else cat >&2; fi
+  } | bsdtar -xvf -
+else
+  zip="${movie%.*}.subs.zip"
+  ! [ -e "$zip" ] || { echo "error: tempfile exists: ${zip@Q}"; exit 1; }
+  if ! "${curl[@]}" -G --fail-with-body -o "$zip" "${curl_data[@]}" "$server_url"; then
+    cat "$zip" && rm "$zip" # zip contains the error message
+  else
+    unzip "$zip" && rm "$zip"
+  fi
+fi
+popd >/dev/null
+shift
+done
